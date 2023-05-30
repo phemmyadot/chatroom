b45a36e93c8da0daa28736e10c614e45ca3e8b8a
Babafemi Adojutelegan <badojutelegan@eminenttechnology.com>
2023-05-29 21:50:32 -0500

    removed topic model

--- base/admin.py
+++ base/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Room, Topic, Message
+from .models import Room, Message
 
 admin.site.register(Room)
-admin.site.register(Topic)
 admin.site.register(Message)
