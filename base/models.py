76c596949a818e986291da20cacc00da82fc014d
Babafemi Adojutelegan <badojutelegan@eminenttechnology.com>
2023-05-29 21:11:39 -0500

    Initial Commit

---
+++ base/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    # updatedBy = models.CharField(max_length=200)
+    updatedAt = models.DateTimeField(auto_now=True)
+    # createdBy = models.CharField(max_length=200)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # members = 
+    # updatedBy = models.CharField(max_length=200)
+    updatedAt = models.DateTimeField(auto_now=True)
+    # createdBy = models.CharField(max_length=200)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updatedAt', '-createdAt']
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    # updatedBy = models.CharField(max_length=200)
+    updatedAt = models.DateTimeField(auto_now=True)
+    # createdBy = models.CharField(max_length=200)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.body[0:50]
